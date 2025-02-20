06c8954fafd3221bf7d12eb037e2550236794005
webfusion2024 <webdevaccfusion@gmail.com>
2025-02-20 14:48:19 +0530

    latest with static

--- webproject/settings.py
+++ webproject/settings.py
@@ -49,10 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'webproject.urls'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
